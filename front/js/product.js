0dcf2b6afac0a3a6a0172002eac1298158302caf
EllanMXM <maxime.jamard@gmail.com>
2022-07-04 15:00:13 +0200

    Local storage deplacer dans Utils

--- front/js/product.js
+++ front/js/product.js
@@ -1,24 +1,3 @@
-const getCart = () => {
-  try {
-    let cart = JSON.parse(localStorage.getItem('cart'));
-
-    if (cart != null && Object.keys(cart).length) {
-      return cart;
-    }
-
-    return {};
-  } catch (error) {
-    return {};
-  }
-}
-
-//Récupération de l'id via les paramètres de l'url
-//const idProduct = new URL(window.location.href).searchParams.get("id");
-
-//fetch("http://localhost:3000/api/products" + idProduct)
-// .then((response) => response.json())
-// .then((response) => {});
-
 //allez chercher les paramètre dans l'url
 let urlparam = new URLSearchParams(window.location.search);
 let idProduct = urlparam.get("id");
@@ -49,7 +28,6 @@ fetch("http://localhost:3000/api/products/" + idProduct)
   description.textContent = data.description;
 
   //Ajout des couleurs
-
   for (let color of data.colors) {
     let optionTag = document.createElement("option");
     document.getElementById("colors").appendChild(optionTag);
@@ -72,9 +50,10 @@ fetch("http://localhost:3000/api/products/" + idProduct)
         
     if (choixCouleur == "") {
       alert("Veuillez sélectionner une couleur.");
-
+      return;
     } else if (choixQuantite <= 0 || choixQuantite >= 100) {
       alert("Nombre d'articles incorrect.");
+      return;
     } 
     else {
       let addedArticle = {
@@ -83,38 +62,51 @@ fetch("http://localhost:3000/api/products/" + idProduct)
         quantity: selectQuantity.value,
       };
 
-      const cart = getCart();
+      let cart = getCart();
       const productKey = idProduct + selectColors.value;
-
+     
+ /*
+      let exampleArray = ['poijfzepoiefzj', 'pojfepoezfj,', 45];console.log(exampleArray[2]);
+      let exampleObject = {
+        name:'nom',
+        quantity: 4
+      }; 
+      let targetKey = 'name';
+      exampleObject.name == exampleObject['name'] == exampleObject[targetKey]
+      {
+        'idproduit plus sa couleur' : {
+          id: 'azdjhdamzdojhia',
+          quantity: 30,
+          color: 'Red'
+        }
+      }
+      exampleObject['name'] = 'toto';
+*/
       if (cart.hasOwnProperty(productKey)) { // Ici la clé existe, donc on modifie la quantité
         // cart[productKey].quantity = nouvelleQuantité (addition de la quantité enregistrée avec la nouvelle quantité saisie)
         // Attention, la quantité totale ne doit pas dépasser 100
+ 
+        let newQuantity = parseInt(addedArticle.quantity) + parseInt(cart[productKey].quantity);
 
-        
+        //if (newQuantity > 0 || newQuantity <= 100)
+        if (newQuantity > 0 && newQuantity <= 100) {
+          cart[productKey].quantity = newQuantity;
+          
+          localStorage.setItem("cart", JSON.stringify(cart));
+        } else {
+          alert("Nombre maximum d'article dépassé")
+          //gérer message d'erreur, nombre maximum dépassé
+        }
       } else { // la clé n'existe pas donc on ajoute le produit au panier
-        cart[productKey] = addedArticle;
+          //La quantité est validée plus haut, pas besoin de faire un nouveau test dans le cas d'un nouveau produit
+          cart[productKey] = addedArticle;
+          localStorage.setItem('cart', JSON.stringify(cart));
       }
-
-      // enregistrer cart dans le localStorage
-    }
-  })
+  }})
 })
+
 .catch(error => {
   // traiter l'erreur
+  console.log(error);
 });
 
-
-
-// addArticle.image == addArticle['image']
-/*
-monPanier[idProduit + selectedColor] => cible un produit du panier (si il existe)
-
-Si la clé existe :
-modifier le quantité
-Sinon: 
-ajouter le produit
-
-Pour tester si une clé existe :
-key in monPanier
-monPanier.hasOwnProperty(key)
-*/ 
